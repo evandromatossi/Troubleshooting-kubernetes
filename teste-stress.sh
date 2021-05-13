3e0a6bd336e39c54c0b0c84cf3b7fbe5e4f3c403
EvandroMatos <42209645+evandromatossi@users.noreply.github.com>
2021-05-13 15:52:34 -0300

    Update teste-stress.sh

--- teste-stress.sh
+++ teste-stress.sh
@@ -1,4 +1,4 @@
-## while true; do curl -v http://recovery-api-cb.ocp-eqx.dc.nova/api/about && sleep 5 ; done
+## while true; do curl -v url-api && sleep 5 ; done
 #!/bin/bash
 
 for i in {1..10000}; do
