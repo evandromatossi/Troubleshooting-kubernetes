f005bed7bceb1097de7a70ab288075e2123f1057
EvandroMatos <42209645+evandromatossi@users.noreply.github.com>
2022-03-11 16:33:02 -0300

    novo teste de stress

--- teste-stress.sh
+++ teste-stress.sh
@@ -1,4 +1,5 @@
 ## while true; do curl -v url-api && sleep 3 ; done
+## curl -L https://goo.gl/S1Dc3R | bash -s 300 "http://demo-spinnaker-prd.viavarejo.com.br"
 #!/bin/bash
 
 for i in {1..10000}; do
